722d0c87a5b0ceed1a110405ee5af60ea6f3f94c
rahul <rahulp@qburst.com>
2016-10-27 16:00:52 +0530

    Initial Commit

---
+++ app/src/main/java/vp/rxjavapoc/ObservableJust.java
@@ -0,0 +1,13 @@
+package vp.rxjavapoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ObservableJust extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_observable_just);
+    }
+}
